762f8550a4a94803ef278f377906597e87e8ade9
altafc22 <altafc22@gmail.com>
2018-10-11 12:48:22 +0530

    logo updated final
    फायनली लोगो अपडेट झाले...

--- edit_company.php
+++ edit_company.php
@@ -83,10 +83,10 @@ if(isset($_POST['update1'])){
     <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
         <a class="navbar-brand brand-logo" href="#">
-          <img src="images/logo.svg" alt="logo" />
+          <img src="images/logo.png" alt="logo" />
         </a>
         <a class="navbar-brand brand-logo-mini" href="#">
-          <img src="images/logo-mini.svg" alt="logo" />
+          <img src="images/logo-mini.png" alt="logo" />
         </a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center">
--- my_account.php
+++ my_account.php
@@ -273,10 +273,10 @@ function checkPass(){
     <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
         <a class="navbar-brand brand-logo" href="#">
-          <img src="images/logo.svg" alt="logo" />
+          <img src="images/logo.png" alt="logo" />
         </a>
         <a class="navbar-brand brand-logo-mini" href="#">
-          <img src="images/logo-mini.svg" alt="logo" />
+          <img src="images/logo-mini.png" alt="logo" />
         </a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center">
