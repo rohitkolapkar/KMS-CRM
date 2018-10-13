7192b43c3ebe514af6941f5511f96c27965b27cd
altafc22 <altafc22@gmail.com>
2018-10-13 11:54:21 +0530

    Update update_complaint.php

--- update_complaint.php
+++ update_complaint.php
@@ -47,9 +47,6 @@ $complaint_query="
 						complaint_details.warranty_status,
 						complaint_details.model_serial_no,
 						complaint_details.problem_description,
-						complaint_details.complaint_close_date,
-						complaint_details.technician_remarks,
-						complaint_details.service_charge,
 						customer_details.customer_name,
 						customer_details.customer_mobile,
 						customer_details.customer_email,
@@ -76,37 +73,29 @@ $complaint_query="
 			$complaint_run=mysqli_query($dbcon,$complaint_query);
 			while($complaint_row=mysqli_fetch_array($complaint_run))
 			{
-				$complaint_id1=$complaint_row[0];
+				$complaint_id=$complaint_row[0];
 				$complaint_date=$complaint_row[1];
 				$complaint_status=$complaint_row[2];
 				$warranty_status=$complaint_row[3];
 				$model_serial_no=$complaint_row[4];
 				$problem_description=$complaint_row[5];
-				$complaint_close_date1=$complaint_row[6];
-				$technician_remark1=$complaint_row[7];
-				$service_charge1=$complaint_row[8];
-				$customer_name=$complaint_row[9];
-				$customer_mobile=$complaint_row[10];
-				$customer_email=$complaint_row[11];
-				$customer_address=$complaint_row[12];
-				$company_name=$complaint_row[13];
-				$company_id=$complaint_row[14];
-				$category_name=$complaint_row[15];
-				$category_id=$complaint_row[16];
-				$model_name=$complaint_row[17];
-				$model_id=$complaint_row[18];
-				$employee_name=$complaint_row[19];
-				$employee_id=$complaint_row[20];
+				$customer_name=$complaint_row[6];
+				$customer_mobile=$complaint_row[7];
+				$customer_email=$complaint_row[8];
+				$customer_address=$complaint_row[9];
+				$company_name=$complaint_row[10];
+				$company_id=$complaint_row[11];
+				$category_name=$complaint_row[12];
+				$category_id=$complaint_row[13];
+				$model_name=$complaint_row[14];
+				$model_id=$complaint_row[15];
+				$employee_name=$complaint_row[16];
+				$employee_id=$complaint_row[17];
 			}
 
 //update code start
 if(isset($_POST['update_complaint'])){
-<<<<<<< HEAD
   
-=======
-	
-  $complaint_number=$_POST['complaint_ID1'];
->>>>>>> c664229e9cdfabab7ca76854421e0734600dcf3a
   $company_id=$_POST['company'];
   $category_id=$_POST['category'];
   $model_id=$_POST['product'];
@@ -119,11 +108,8 @@ if(isset($_POST['update_complaint'])){
   $service_charge=$_POST['service_charge'];
   $closing_date=$_POST['closing_date'];
   $complaint_status=$_POST['complaint_status'];
-<<<<<<< HEAD
   $complaint_id=$_POST['complaint_id'];
   //echo $complaint_status.",,,,".$closing_date.",,,,".$technician_remark.",,,,".$service_charge;
-=======
->>>>>>> c664229e9cdfabab7ca76854421e0734600dcf3a
 
  $query="UPDATE complaint_details SET
     company_id='$company_id',
@@ -139,20 +125,16 @@ if(isset($_POST['update_complaint'])){
     technician_remarks='$technician_remark',
     service_charge='$service_charge'
 WHERE
-<<<<<<< HEAD
     complaint_id='$complaint_id'";
-=======
-    complaint_id='$complaint_number'";
->>>>>>> c664229e9cdfabab7ca76854421e0734600dcf3a
 	
 if(mysqli_query($dbcon,$query))
 {
-  echo "<script>alert('Complaint has been updated')</script>";
-  echo "<script>window.open('search_complaint.php','_self')</script>";
+  echo "<script>alert('Done')</script>";
+  //echo "<script>window.open('search_complaint.php','_self')</script>";
 }
 else
 {
- echo "<script>alert('Complaint update failed')</script>";
+ echo "<script>alert('Update Failed')</script>";
 }
 
 }
@@ -469,20 +451,7 @@ $("#suggesstion-box").hide();
                       <div class="col-md-6">
 
                         <h4 class="card-title">
-<<<<<<< HEAD
                               Cutomer Details
-=======
-                              Cutomer Details &nbsp;&nbsp;   
-							  
-                        
-                          <label class="col-sm-3 col-form-label">Complaint ID*</label>
-                          
-                            <select class="card-title" name="complaint_ID1" readonly >
-								<option value="<?php echo $complaint_id; ?>"><?php echo $complaint_id; ?></option>
-                            </select>
-                          
-                    
->>>>>>> c664229e9cdfabab7ca76854421e0734600dcf3a
                         </h4>
                       </div>  
                     </div>
@@ -499,7 +468,7 @@ $("#suggesstion-box").hide();
                         <div class="form-group row">
                           <label class="col-sm-3 col-form-label">Complaint Date</label>
                           <div class="col-sm-9">
-                            <input class="form-control" type="date" id="cdate"  name="cdate" value="<?php echo $complaint_date;?>" disabled />
+                            <input class="form-control" type="date" id="cdate"  name="cdate" value="<?php echo $complaint_date;?>" max="<?php echo $today;?>" disabled />
                           </div>
                         </div>
                       </div>
@@ -644,7 +613,7 @@ $("#suggesstion-box").hide();
                         <div class="form-group row">
                           <label class="col-sm-3 col-form-label">Tecnician Remark*</label>
                           <div class="col-sm-9">
-                            <input type="text" class="form-control" name="technician_remark" value="<?php echo $technician_remark1; ?>" placeholder="Tecnician Remark*" />
+                            <input type="text" class="form-control" name="technician_remark" value="" placeholder="Tecnician Remark*" />
                           </div>
                         </div>
                       </div>
@@ -656,7 +625,7 @@ $("#suggesstion-box").hide();
 				  <div class="form-group row">
 				  <label class="col-sm-3 col-form-label">Service Charge</label>
 				  <div class="col-sm-9">
-					<input type="number" class="form-control" name="service_charge" value="<?php echo $service_charge1; ?>" placeholder="Rs." />
+					<input type="number" class="form-control" name="service_charge" value="" placeholder="Rs." />
 				  </div>
 				  </div>
 				  </div>
@@ -666,7 +635,7 @@ $("#suggesstion-box").hide();
                         <div class="form-group row">
                           <label class="col-sm-3 col-form-label">Closing Date</label>
                           <div class="col-sm-9">
-                            <input class="form-control" type="date" id="closing_date"  name="closing_date" value="<?php echo $complaint_close_date1; ?>" />
+                            <input class="form-control" type="date" id="closing_date"  name="closing_date" value="<?php echo $today; ?>" />
                           </div>
                         </div>
                       
@@ -675,11 +644,7 @@ $("#suggesstion-box").hide();
 				  
 				  <div class="col-md-6">
 				        <div class="form-group row">
-<<<<<<< HEAD
                           <label class="col-sm-3 col-form-label">Complaint Status* </label>
-=======
-                          <label class="col-sm-3 col-form-label">Complaint Status*</label>
->>>>>>> c664229e9cdfabab7ca76854421e0734600dcf3a
                           <div class="col-sm-9">
                             <select class="form-control" name="complaint_status" id="complaint_status"  >
                               <option selected="selected" value="<?php echo $complaint_status; ?>"><?php echo $complaint_status; ?></option>
